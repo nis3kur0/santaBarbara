7c100a87e279ad2a5dfe25c02ad36483b323ad35
Gabo <gabriel.gonzalez@linktic.com>
2025-01-10 23:30:14 -0400

    primer commit

---
+++ src/main/java/com/mycompany/santabarbara/SantaBarbara.java
@@ -0,0 +1,16 @@
+/*
+ * Click nbfs://nbhost/SystemFileSystem/Templates/Licenses/license-default.txt to change this license
+ */
+
+package com.mycompany.santabarbara;
+
+/**
+ *
+ * @author gabo
+ */
+public class SantaBarbara {
+
+    public static void main(String[] args) {
+        System.out.println("Hello World!");
+    }
+}
